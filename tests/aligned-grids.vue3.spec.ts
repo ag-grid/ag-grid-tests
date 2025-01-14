92c86dc8e4f2210e64e6b8834c135d697b25b0c5
seanlandsman <sean@thelandsmans.com>
2025-01-14 06:37:55 +0000

    Add wait

--- tests/aligned-grids.vue3.spec.ts
+++ tests/aligned-grids.vue3.spec.ts
@@ -12,8 +12,9 @@ test('aligned grids', async ({ page }) => {
 
   await page.goto('http://127.0.0.1:8085/vue3/aligned-grids/');
 
-  const firstCell = page.locator('.ag-cell-value').first();
-  await firstCell.waitFor();
+  await page.waitForTimeout(2000); // waits for 2 seconds
+  // const firstCell = page.locator('.ag-cell-value').first();
+  // await firstCell.waitFor();
 
   expect(await page.locator('span.ag-icon-contracted:visible').count()).toBe(2);
 

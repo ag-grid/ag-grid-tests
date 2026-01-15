60261940e5593e710aa81a0143d848d61aa4bad8
seanlandsman <sean@thelandsmans.com>
2026-01-15 07:33:26 +0000

    .

--- tests/AG-14134-class-instance-rowdata.spec.ts
+++ tests/AG-14134-class-instance-rowdata.spec.ts
@@ -5,6 +5,9 @@ test('reactive rowdata', async ({ page }) => {
 
   await page.goto('http://localhost:8085/vue3/AG-14134-class-instance-rowdata/');
 
+  const firstCell = page.locator('.ag-cell-value').first();
+  await firstCell.waitFor();
+
   expect(await page.getByLabel('cell-value').textContent()).toBe('name 1-Mission: name 1-true');
   expect(await page.getByLabel('selection-check').textContent()).toBe('');
 

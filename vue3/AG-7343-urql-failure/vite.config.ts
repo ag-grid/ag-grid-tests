d906e96bec9ba7e25a2a8e99cbff449a028bf31b
seanlandsman <sean@thelandsmans.com>
2026-01-27 20:34:09 +0000

    .

--- vue3/AG-7343-urql-failure/vite.config.ts
+++ vue3/AG-7343-urql-failure/vite.config.ts
@@ -3,5 +3,21 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  optimizeDeps: { // 👈 optimizedeps
+    esbuildOptions: {
+      target: "esnext",
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      supported: {
+        bigint: true
+      },
+    }
+  },
+  build: {
+    target: ["esnext"], // 👈 build.target
+  },
+
 })

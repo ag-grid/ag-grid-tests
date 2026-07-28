f0335b359181f4a0b4ea8aa673addbb0866094f3
seanlandsman <sean@thelandsmans.com>
2026-07-28 20:05:07 +0100

    .

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ do
   cd $subDirectory
   cp ../../update.sh .
   rm -rf node_modules
-  npm i --no-package-lock --registry https://registry.ag-grid.com/
+  npm i --no-package-lock --legacy-peer-deps --registry https://registry.ag-grid.com/
   if [ $? -ne 0 ];
   then
     pwd
